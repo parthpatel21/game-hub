fe4b7b1de0ce222e339fca7943efdabf6f59e906
parthpatel21 <patel21998@gmail.com>
2023-10-15 19:15:33 +0530

    Color Mode switch Added

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,13 @@ function App() {
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area={"nav"} bg={"lightblue"}>
+      <GridItem area={"nav"}>
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"} bg={"yellow"}>
-          Aside
-        </GridItem>
+        <GridItem area={"aside"}>Aside</GridItem>
       </Show>
-      <GridItem area={"main"} bg={"pink"}>
-        Main
-      </GridItem>
+      <GridItem area={"main"}>Main</GridItem>
     </Grid>
   );
 }
